3c6d1e91cbeca56862dad670346eaebe183bbb94
kottor <gardien243@gmail.com>
2025-03-13 16:12:24 +0100

    couleur varié

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,38 @@
 let sectionPlat=document.querySelector("#plat")
-let sectionDessert=document.querySelector("#dessert")
-let sectionBoisson=document.querySelector('#boisson')
+let sectionDessert=document.querySelector("#section_dessert")
+let sectionBoisson=document.querySelector('#section_boisson')
 let plat=sectionPlat.querySelectorAll('.card')
 let dessert=sectionDessert.querySelectorAll('.card')
-let boisson=sectionPlat.querySelectorAll('.card')
+let boisson=sectionBoisson.querySelectorAll('.card')
 let carte=document.querySelectorAll('.card')
 var lol
 corp_carte=document.querySelectorAll('.card-body')
 carte.forEach(element => {
-    lol=element.querySelector('.card-body')
-    console.log(element);
-    console.log(lol);
-    lol.addEventListener('click', ()=>{
-        element.style.boxShadow = "0.1cm 0.1cm 0.1cm red"
-    })
+    
     element.addEventListener('mouseover', ()=>{
         element.style.backgroundColor="gray"
     })
     element.addEventListener('mouseout', ()=>{
         element.style.backgroundColor="white"
     })
-    
 });
+plat.forEach(element => {
+    element.addEventListener('click', ()=>{
+        element.style.boxShadow = "0.1cm 0.1cm 0.1cm blue"
+    })
+});
+
+dessert.forEach(element => {
+    element.addEventListener('click', ()=>{
+        element.style.boxShadow = "0.1cm 0.1cm 0.1cm green"
+    })
+});
+boisson.forEach(element => {
+    element.addEventListener('click', ()=>{
+        element.style.boxShadow = "0.1cm 0.1cm 0.1cm red"
+    })
+});
+
 
 
 
