cbd794ae8597fbd54882816f1443720e4e452015
kottor <gardien243@gmail.com>
2025-03-13 15:35:10 +0100

    couleur

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,27 @@
-let sectionPlat=document.querySelector(".plat")
-let sectionDessert=document.querySelector(".dessert")
-let sectionBoisson=document.querySelector('.boisson')
-let plat=sectionPlat.querySelector('.card')
-let dessert=sectionDessert.querySelector('.card')
-let boisson=sectionPlat.querySelector('.card')
+let sectionPlat=document.querySelector("#plat")
+let sectionDessert=document.querySelector("#dessert")
+let sectionBoisson=document.querySelector('#boisson')
+let plat=sectionPlat.querySelectorAll('.card')
+let dessert=sectionDessert.querySelectorAll('.card')
+let boisson=sectionPlat.querySelectorAll('.card')
+let carte=document.querySelectorAll('.card')
+var lol
+corp_carte=document.querySelectorAll('.card-body')
+carte.forEach(element => {
+    lol=element.querySelector('.card-body')
+    console.log(element);
+    console.log(lol);
+    lol.addEventListener('click', ()=>{
+        element.style.boxShadow = "0.1cm 0.1cm 0.1cm red"
+    })
+    element.addEventListener('mouseover', ()=>{
+        element.style.backgroundColor="gray"
+    })
+    element.addEventListener('mouseout', ()=>{
+        element.style.backgroundColor="white"
+    })
+    
+});
+
+
+
